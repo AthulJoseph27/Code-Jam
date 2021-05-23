fcab3e58b69cf4eed8fb20f7d3e77403a1acf517
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2021-05-23 22:44:24 +0530

    Create AlienGenerator.cpp

---
+++ Kick Start/2021/1 C/AlienGenerator.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define all(v) v.begin(), v.end()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    long long G;
+    cin >> G;
+
+    long long limit = round(sqrt(2 * G));
+
+    long long count = 0;
+
+    for (long long i = 0; i <= limit; i++)
+    {
+
+        long long num = (2 * G) - (i * (i + 1));
+        long long deno = 2 * (i + 1);
+        // debug(i, num, deno, num / deno, num % deno);
+        if (num <= 0)
+            break;
+
+        if (num % deno == 0)
+        {
+            count++;
+        }
+    }
+
+    cout << "Case #" << cc << ": " << count << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
